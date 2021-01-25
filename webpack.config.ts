66dc4ae8a381b120d4814cee88fb40dc697ada5c
Josh.Siegl <Josh.Siegl@ebshealthcare.com>
2021-01-25 15:24:53 -0500

    changes

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,28 @@
 import * as path from 'path'; 
-import * as webpack from 'webpack'; 
+import * as webpack from 'webpack';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin'; 
 
 const config: webpack.Configuration = { 
     mode: 'production', 
-    entry: './index.ts', 
+    entry: './index.tsx', 
     output: { 
         path: path.resolve(__dirname, 'build'), 
         filename: 'webpack.bundle.js'
+    }, 
+    resolve: { 
+        extensions: ['.tsx', '.ts', '.js']
+    }, 
+    plugins: { 
+        new HtmlWebpackPlugin
+    }, 
+    module: { 
+        rules: [ 
+            { 
+                test: /\.tsx?$/, 
+                use: 'ts-loader',
+                exclude: /node_modules/ 
+            }
+        ]
     }
 }
 
